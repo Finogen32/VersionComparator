9ecca9f82b3b02080dee23e143f9d87f51d2f86f
DmitriyFinogenov <FIN519@yandex.ru>
2024-01-15 21:12:46 +0300

    -add tests

--- src/test/java/ComparatorTest.java
+++ src/test/java/ComparatorTest.java
@@ -1,5 +1,28 @@
+import jdk.jfr.Name;
+import org.classes.Version;
+import org.junit.Assert;
 import org.junit.Test;
 
+import static org.classes.CompareVersion.compareVersion;
+
 public class ComparatorTest {
+    Version v1 = new Version("10.0.12.1.3.13");
+    Version v2 = new Version("10.0.13");
+    Version v3 = new Version("10.0.13");
+
+    @Test
+    @Name("Проверка сравнения версий")
+    public void checkComparatorVersion(){
+        Assert.assertFalse(
+                compareVersion(v1.getNumberArray(),v2.getNumberArray(), ">"));
+        Assert.assertFalse(
+                compareVersion(v1.getNumberArray(),v2.getNumberArray(), ">="));
+        Assert.assertTrue(
+                compareVersion(v1.getNumberArray(),v2.getNumberArray(), "<"));
+        Assert.assertTrue(
+                compareVersion(v1.getNumberArray(),v2.getNumberArray(), "<="));
+        Assert.assertTrue(
+                compareVersion(v2.getNumberArray(),v3.getNumberArray(), "="));
+    }
 
 }
